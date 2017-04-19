ed3f291a619805eabfb7a03372cb197ff881df2d
netblitzer <thenetblitzer@gmail.com>
2017-04-19 11:45:08 -0400

    Work on Enemy/Yeti r1

---
+++ Assets/Prototyping/Scripts/PathNode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode : MonoBehaviour {
+
+    void OnDrawGizmos () {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(gameObject.transform.position, 1);
+    }
+}
