4567c10e3b3b1296987947eaa742566ab855560b
Julia <jew3873@g.rit.edu>
2017-04-30 15:59:51 -0400

    trying to get pause menu to work

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    bool paused;
+
 	// Use this for initialization
 	void Start () {
-		
+        paused = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Escape))
         {
-
+            TogglePause();
         }
 	}
+
+    void OnGUI()
+    {
+        if(paused)
+        {
+            //if it is paused, show pause menu
+            GUILayout.Label("Game is paused!");
+        }
+    }
+
+    void TogglePause()
+    {
+        if (paused)
+            paused = false;
+        if (!paused)
+            paused = true;
+    }
 }
