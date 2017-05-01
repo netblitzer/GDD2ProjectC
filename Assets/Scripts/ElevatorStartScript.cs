bf48419aa0af33b85231b1eb068a13ad594e0eca
Julia <jew3873@g.rit.edu>
2017-04-30 21:54:13 -0400

    pause menu still dark but its working and elevator is moving

---
+++ Assets/Scripts/ElevatorStartScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorStartScript : MonoBehaviour {
+
+    bool elevator;
+    Vector3 startingposition;
+    Vector3 endingposition;
+    float startTime;
+    private float journeyLength;
+
+    // Use this for initialization
+    void Start () {
+        elevator = true;
+        startingposition = new Vector3(0.05f, 8.83f, 34.22f);
+        endingposition = new Vector3(0.05f, -0.25f, 34.41f);
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startingposition, endingposition);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(elevator == true && transform.position != endingposition)
+        {
+            float speed = 1.0F;
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            Vector3 lerp = Vector3.Lerp(startingposition, endingposition, fracJourney);
+            transform.position = lerp;
+            print("supposed to be moving");
+        }
+	}
+}
