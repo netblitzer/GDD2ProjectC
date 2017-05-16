5de2c8273ff5a5f5e79b7707b08d7b98a4c77ff4
Mike Campito <Mikecampito@gmail.com>
2017-05-16 18:49:07 -0400

    Added ending.  NumFol is the number of following yetis, that carries over between scenes for display at the end scene.

---
+++ Assets/Scripts/EndTransition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTransition : MonoBehaviour {
+    public static float numFol=0;
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject[] following = GameObject.FindGameObjectsWithTag("Yeti");
+        if (following.Length > 0)
+        {
+            for (int i = 0; i < following.Length; i++)
+            {
+                if (following[i].GetComponent<YetiFlocker>().following)
+                {
+                    numFol++;
+                }
+            }
+        }
+        SceneManager.LoadScene("EndScene");
+
+    }
+}
