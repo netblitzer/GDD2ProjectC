80102d9e18742f71dd091578da85ade3ff8bfdf5
Matthew Pressman <mrp3362@rit.edu>
2017-05-16 20:39:20 -0400

    Changed the mini map to not rotate with the player

---
+++ Assets/Scripts/MinimapCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour {
+    public GameObject player;
+
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 newPos = player.transform.position;
+        newPos.y += 40;
+
+        transform.position = newPos;
+	}
+}
